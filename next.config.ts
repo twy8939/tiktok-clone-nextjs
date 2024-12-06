d1b4eb9033c5ca4c8644a768167f9cabc23df13c
twy8939 <twy8939@naver.com>
2024-12-07 00:03:17 +0900

    init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: "raw-loader",
+    });
+
+    if (!isServer) config.externals.push("canvas");
+    return config;
+  },
 };
 
 export default nextConfig;
