538d1f79232481f77aaee70de4192e1c2796c33f
twy8939 <twy8939@naver.com>
2024-12-09 22:43:06 +0900

    feat: create TopNav with responsive design

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import TopNav from "./includes/TopNav";
 
 export default function MainLayout({
   children,
@@ -10,6 +11,7 @@ export default function MainLayout({
   const pathname = usePathname();
   return (
     <>
+      <TopNav />
       <div
         className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${
           pathname === "/" ? "max-w-[1140px]" : ""
