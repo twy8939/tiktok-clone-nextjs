e4c3e996d1b4ae0c4ea4c13533449e056f45724f
twy8939 <twy8939@naver.com>
2024-12-13 23:21:55 +0900

    feat: add PostMain component

--- app/types.tsx
+++ app/types.tsx
@@ -1,10 +1,28 @@
-// LAYOUT INCLUDE TYPES
-
 export interface RandomUsers {
   id: string;
   name: string;
   image: string;
 }
+
+// COMPONENT TYPES
+
+export interface PostMainCompTypes {
+  post: PostWithProfile;
+}
+
+export interface PostWithProfile {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  create_at: string;
+  profile: {
+    user_id: string;
+    name: string;
+    image: string;
+  };
+}
+// LAYOUT INCLUDE TYPES
 
 export interface MenuItemsTypes {
   iconString: string;
