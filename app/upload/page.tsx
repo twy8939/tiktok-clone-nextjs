ce56aac133b1dc506a7edafcdb6e84758a9731ed
twy8939 <twy8939@naver.com>
2024-12-16 23:34:56 +0900

    feat: add post actions & editing options

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import { UploadError } from "../types";
 import UploadLayout from "../layouts/UploadLayout";
 import { BiLoaderCircle, BiSolidCloudUpload } from "react-icons/bi";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { PiKnifeLight } from "react-icons/pi";
 
 export default function Upload() {
   const router = useRouter();
@@ -25,6 +27,21 @@ export default function Upload() {
       setFileDisplay(fileUrl);
       setFile(file);
     }
+  };
+
+  const discard = () => {
+    setFileDisplay("");
+    setFile(null);
+    setCaption("");
+  };
+
+  const clearVideo = () => {
+    setFileDisplay("");
+    setFile(null);
+  };
+
+  const createNewPost = () => {
+    console.log("createNewPost");
   };
 
   return (
@@ -100,8 +117,93 @@ export default function Upload() {
                   className="absolute rounded-xl object-cover z-10 p-[13px] w-full h-full"
                   src={fileDisplay}
                 />
+
+                <div className="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
+                  <div className="flex items-center truncate">
+                    <AiOutlineCheckCircle size="16" className="min-w-[16px]" />
+                    <p className="text-[11px] pl-1 truncate text-ellipsis">
+                      {file ? file.name : ""}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() => clearVideo()}
+                    className="text-[11px] ml-2 font-semibold"
+                  >
+                    Change
+                  </button>
+                </div>
               </div>
             )}
+
+            <div className="mt-4 mb-6">
+              <div className="flex bg-[#F8F8F8] py-4 px-6">
+                <div>
+                  <PiKnifeLight size="20" className="mr-4" />
+                </div>
+                <div>
+                  <div className="text-semibold text-[15px] mb-1.5">
+                    Divide videos and edit
+                  </div>
+                  <div className="text-semibold text-[13px] text-gary-400">
+                    You can quickly divide videos into multiple parts, remove
+                    redundant parts, and turn landscape videos into portrait
+                    videos.
+                  </div>
+                </div>
+                <div className="flex justify-end max-w-[130px] w-full h-full text-center my-auto">
+                  <button className="px-8 py-1.5 text-white text-[15px] bg-[#F02C56] rounded-sm">
+                    Edit
+                  </button>
+                </div>
+              </div>
+
+              <div className="mt-5">
+                <div className="flex items-center justify-between">
+                  <div className="mb-1 text-[15px]">Caption</div>
+                  <div className="text-gray-400 text-[12px]">
+                    {caption.length}/150
+                  </div>
+                </div>
+                <input
+                  maxLength={150}
+                  type="text"
+                  className="w-full border p-2.5 rounded-md focus:outline-none"
+                  value={caption}
+                  onChange={(event) => setCaption(event.target.value)}
+                />
+              </div>
+
+              <div className="flex gap-3">
+                <button
+                  disabled={isUploading}
+                  onClick={() => discard()}
+                  className="px-10 py-2.5 mt-8 border text-[16px] hover:bg-gray-100 rounded-sm"
+                >
+                  Discard
+                </button>
+                <button
+                  disabled={isUploading}
+                  onClick={() => createNewPost()}
+                  className="px-10 py-2.5 mt-8 border text-[16px] text-white bg-[#F02C56] rounded-sm"
+                >
+                  {isUploading ? (
+                    <BiLoaderCircle
+                      className="animate-spin"
+                      color="#ffffff"
+                      size={25}
+                    />
+                  ) : (
+                    "Post"
+                  )}
+                </button>
+              </div>
+
+              {error ? (
+                <div className="text-red-600" mt-4>
+                  {error.message}
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </UploadLayout>
