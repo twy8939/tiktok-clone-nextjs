1839711a5786f4ef3958e9f65fed3567723ff43b
twy8939 <twy8939@naver.com>
2025-01-12 18:23:39 +0900

    feat: add useDeletePostById hook

---
+++ app/hooks/useDeletePostById.tsx
@@ -0,0 +1,34 @@
+import { database, storage } from "@/libs/AppWriteClient";
+import useGetLikesByPostId from "./useGetLikesByPostId";
+import useDeleteLike from "./useDeleteLike";
+import useGetCommentsByPostId from "./useGetCommentsByPostId";
+import useDeleteComment from "./useDeleteComment";
+
+const useDeletePostById = async (postId: string, currentImage: string) => {
+  try {
+    const likes = await useGetLikesByPostId(postId);
+    likes.forEach(async (like) => {
+      await useDeleteLike(like?.id);
+    });
+
+    const comments = await useGetCommentsByPostId(postId);
+    comments.forEach(async (comment) => {
+      await useDeleteComment(comment?.id);
+    });
+
+    await database.deleteDocument(
+      String(process.env.NEXT_PUBLIC_DATABASE_ID),
+      String(process.env.NEXT_PUBLIC_COLLECTION_ID_LIKE),
+      postId
+    );
+
+    await storage.deleteFile(
+      String(process.env.NEXT_PUBLIC_BUCKET_ID),
+      currentImage
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default useDeletePostById;
